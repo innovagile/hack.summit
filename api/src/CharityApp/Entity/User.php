d8734b0901397e6b0ffa236c0f64587916698431
Łukasz Chełmicki <lukasz.chelmicki@gmail.com>
2016-02-21 21:09:53 +0100

    User Entity

---
+++ api/src/CharityApp/Entity/User.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace CharityApp\Entity;
+
+class User implements \JsonSerializable
+{
+    public $id;
+    public $username;
+    public $password;
+    public $firstName;
+    public $lastName;
+    public $bio;
+
+    /**
+     * User constructor.
+     * @param $id
+     * @param $username
+     * @param $password
+     * @param $firstName
+     * @param $lastName
+     * @param $bio
+     */
+    public function __construct($id, $username, $password, $firstName, $lastName, $bio)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->bio = $bio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBio()
+    {
+        return $this->bio;
+    }
+
+    /**
+     * @param mixed $bio
+     *
+     * @return User
+     */
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'first_name' => $this->firstName,
+            'last_name' => $this->lastName,
+            'bio' => $this->bio,
+        ];
+    }
+}
