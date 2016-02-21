4ecf322bd75b028daebe2b60988de1887cf2d060
duraseb <sszulc@gmail.com>
2016-02-21 14:10:30 +0100

    Organization mock

--- api/src/routes.php
+++ api/src/routes.php
@@ -3,5 +3,6 @@
 
 $app->get('/', 'CharityApp\Action\Home');
 $app->get('/organization', 'CharityApp\Action\Organization');
+$app->get('/organization/{slug}', 'CharityApp\Action\Organization');
 $app->get('/user', 'CharityApp\Action\User');
 $app->get('/cause', 'CharityApp\Action\Cause');
