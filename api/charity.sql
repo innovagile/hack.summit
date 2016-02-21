a380c5843f7c54aedf51dad5733f32ae88876992
duraseb <sszulc@gmail.com>
2016-02-21 19:44:53 +0100

    user and user->org db tables

--- api/charity.sql
+++ api/charity.sql
@@ -8,3 +8,20 @@ CREATE TABLE organization (
   blurb text,
   description text
 );
+
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
+  id bigserial PRIMARY KEY,
+  username character varying(32),
+  password character varying(64),
+  first_name character varying(32),
+  last_name character varying(32),
+  bio text
+);
+
+DROP TABLE IF EXISTS user_organization;
+CREATE TABLE user_organization (
+  id bigserial PRIMARY KEY,
+  user_id bigint NOT NULL,
+  organization_id bigint NOT NULL
+);
