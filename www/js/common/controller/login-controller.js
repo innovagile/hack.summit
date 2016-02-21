fe6e9ee660aa4a24328f2a08aa86885068278a24
ssuperczynski <s.superczynski@gmail.com>
2016-02-21 18:28:56 +0100

    login and register layouts

---
+++ www/js/common/controller/login-controller.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('charityApp')
+        .controller('LoginCtrl', LoginCtrl);
+
+    LoginCtrl.$inject = ['$scope'];
+
+    function LoginCtrl($scope) {
+
+        var login = this;
+
+
+    }
+
+
+}());
