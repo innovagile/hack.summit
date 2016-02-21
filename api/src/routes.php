a99c4771ec802c853ae97ee775e42d93c895b182
duraseb <sszulc@gmail.com>
2016-02-21 21:35:18 +0100

    Fetching users from DB

--- api/src/routes.php
+++ api/src/routes.php
@@ -5,5 +5,6 @@ $app->get('/', 'CharityApp\Action\Home');
 $app->get('/organization', 'CharityApp\Action\Organization');
 $app->get('/organization/{slug}', 'CharityApp\Action\Organization');
 $app->get('/user', 'CharityApp\Action\User');
+$app->get('/user/{username}', 'CharityApp\Action\User');
 $app->get('/cause', 'CharityApp\Action\Cause');
 $app->post('/organization', 'CharityApp\Action\Organization');
